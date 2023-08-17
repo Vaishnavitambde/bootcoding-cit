9fbe9aee90afd1e6ea6a85794e68bdd138e611b8
Vaishnavitambde <vaishnavitambde95@gmail.com>
2023-08-17 15:52:28 +0530

    Java Programs

---
+++ src/java/Conversion.java
@@ -0,0 +1,13 @@
+public class Conversion {
+    public static void main(String[] args) {
+        int hours = 1;
+        int minutes = 1;
+        int seconds = minutes * 60;
+        int hourSeconds = hours * 60 * 60;
+
+        System.out.println("Hour to seconds : " + hourSeconds);
+        System.out.println("Minutes to seconds : " + seconds);
+    }
+}
+
+
